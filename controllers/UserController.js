13d620d6cec56f3411d4d3a7a237ef98d7486b15
Kshitij <kshitijpatil508@gmail.com>
2023-05-02 16:40:14 +0530

    Reset Password

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,73 +1,99 @@
-const UserModel = require("../models/UserModel")
+const UserModel = require("../models/UserModel");
 
 const registerController = async (req, res) => {
-    const { name, email, password, role } = req.body
-    console.log(name, email, password, role)
-    let userobj = {
-        name: name,
-        password: password,
-        email: email,
-        role: role
-    }
+  const { name, email, password, role } = req.body;
+  console.log(name, email, password, role);
+  let userobj = {
+    name: name,
+    password: password,
+    email: email,
+    role: role,
+  };
 
-    try {
-        const mail = await UserModel.find({ email })
+  try {
+    const mail = await UserModel.find({ email });
 
-        if (mail.length !== 0) {
-            console.log(mail)
-            res.json({
-                message: `${email} already exist`
-            })
-        }
-        else {
-            let data = await UserModel(userobj).save()
-            if (data) {
-                return res.json({
-                    message: "data inserted successfully"
-                })
-            }
-            return res.json({
-                message: "data not inserted"
-            })
-        }
-    }
-    catch (error) {
+    if (mail.length !== 0) {
+      console.log(mail);
+      res.json({
+        message: `${email} already exist`,
+      });
+    } else {
+      let data = await UserModel(userobj).save();
+      if (data) {
         return res.json({
-            message: "some error"
-        })
+          message: "data inserted successfully",
+        });
+      }
+      return res.json({
+        message: "data not inserted",
+      });
     }
-}
+  } catch (error) {
+    return res.json({
+      message: "some error",
+    });
+  }
+};
 
 const userLogin = async (req, res) => {
-    const { email, password } = req.body
-    const userpass = {password}
-    try {
-        const data = await UserModel.find({ email })
-        if (data.length !== 0) {
-            // console.log(data)
-            console.log(userpass.password, data[0].password)
-            if(userpass.password == data[0].password){
-                return res.json({
-                    message: `${data[0].name} logged in successfully`
-                })
-            }
-            return res.json({
-                message: `Incorrect password`
-            })
-        }
-        
+  const { email, password } = req.body;
+  const userpass = { password };
+  try {
+    const data = await UserModel.find({ email });
+    if (data.length !== 0) {
+      // console.log(data)
+      console.log(userpass.password, data[0].password);
+      if (userpass.password == data[0].password) {
         return res.json({
-            message: "User doesn't exist or Incorrect email"
-        })
+          message: `${data[0].name} logged in successfully`,
+        });
+      }
+      return res.json({
+        message: `Incorrect password`,
+      });
     }
-    catch (error) {
-        return res.json({
-            message: "error"
-        })
+
+    return res.json({
+      message: "User doesn't exist or Incorrect email",
+    });
+  } catch (error) {
+    return res.json({
+      message: "error",
+    });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const data = await UserModel.findOne({ email: email });
+
+    if (data) {
+      const newPass = password;
+      const userData = await UserModel.findOneAndUpdate(
+        { email: email },
+        { $set: { password: newPass } }
+      );
+      res.json({
+        message: "Password Updated Sucessfully",
+      });
+    } else {
+      res.json({
+        message: "User not found",
+      });
     }
-}
+  } catch (error) {
+    return res.json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
-    registerController,
-    userLogin
-}
+  registerController,
+  userLogin,
+  resetPassword,
+};
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,16 +1,18 @@
-const express = require("express")
-const UserRouter = express.Router()
+const express = require("express");
+const UserRouter = express.Router();
 
-const { 
-    registerController,
-    userLogin
-} = require("../controllers/UserController")
+const {
+  registerController,
+  userLogin,
+  resetPassword,
+} = require("../controllers/UserController");
 
 // const {rights,userbyrole} = require("../controllers/Rightcontroller")
 // UserRouter.post("/right",rights)
 // UserRouter.post("/getuserbyrole",userbyrole)
 
-UserRouter.post("/register",registerController)
-UserRouter.post("/login",userLogin)
+UserRouter.post("/register", registerController);
+UserRouter.post("/login", userLogin);
+UserRouter.post("/resetPassword", resetPassword);
 
-module.exports = UserRouter
+module.exports = UserRouter;
